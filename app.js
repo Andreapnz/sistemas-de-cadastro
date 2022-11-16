3894e3524fc8af167c4e03961f9b18cf1fe9f10b
Andrea Correia <andrea_pnz@hotmail.com>
2022-11-16 17:40:25 -0300

    voltando

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 //mais um console log de teste:
 console.log("fazer um commit de teste")
-cocnsole.log("Enviando para o github")
